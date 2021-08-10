82dd0de24c100a47632d3a6487ae06369f869369
Tiff-C <deguntiffany@gmail.com>
2021-08-10 13:22:11 +0000

    created sendEmail function and tested it works

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,15 @@
-console.log('hello');
+function sendMail(contactForm) {
+  emailjs.send("service_ldrreie","template_n03korg", {
+    'from_name': contactForm.name.value,
+    'from_email': contactForm.emailaddress.value,
+    'project_request': contactForm.projectsummary.value
+  })
+  .then (function(response) {
+    console.log('success', response);
+  },
+  function(error) {
+    console.log('failed', error);
+  });
+
+  return false;
+}
